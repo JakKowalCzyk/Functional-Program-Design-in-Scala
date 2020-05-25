135b16aba6b6272804d0712fd4a0fe5f36830fe5
jakub.kowalczyk <jakub.kowalczyk@br-ag.eu>
2020-05-25 07:50:14 +0200

    week 5 exercises

---
+++ exercises/src/main/scala/week5/Publisher.scala
@@ -0,0 +1,15 @@
+package week5
+
+trait Publisher {
+
+  private var subscribers: Set[Subscriber] = Set()
+
+  def subscribe(subscriber: Subscriber): Unit =
+    subscribers += subscriber
+
+  def unSubscribe(subscriber: Subscriber): Unit =
+    subscribers -= subscriber
+
+  def publish(): Unit =
+    subscribers.foreach(_.handler(this))
+}
