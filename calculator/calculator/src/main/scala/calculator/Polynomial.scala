46b147661f235db7560907ef0ed82e822dbc0779
jakub.kowalczyk <jakub.kowalczyk@br-ag.eu>
2020-05-27 07:13:05 +0200

    week 5 assignment

---
+++ calculator/calculator/src/main/scala/calculator/Polynomial.scala
@@ -0,0 +1,21 @@
+package calculator
+
+object Polynomial extends PolynomialInterface {
+  def computeDelta(a: Signal[Double], b: Signal[Double],
+      c: Signal[Double]): Signal[Double] = {
+    Signal{
+      val bValue = b()
+      (bValue * bValue) - 4*(a()*c())
+    }
+  }
+
+  def computeSolutions(a: Signal[Double], b: Signal[Double],
+      c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
+    Signal{
+      val deltaValue = delta()
+      if(deltaValue < 0) Set.empty
+      else if (deltaValue == 0) Set( (-b()) / (2*a()) )
+      else Set( (-b() - math.sqrt(deltaValue)) / (2*a()), (-b() + math.sqrt(deltaValue)) / (2*a())  )
+    }
+  }
+}
